796449b4095ec60d4fc16edcb39d74ae590dd713
jonny5v <jonny5v@gmail.com>
2018-06-15 16:14:19 -0500

    second commit

--- src/app/positions-list/positions-list.component.ts
+++ src/app/positions-list/positions-list.component.ts
@@ -18,13 +18,11 @@ export class PositionsListComponent implements OnInit {
 
     jobTypes: Array<string> = [];
     locations: Array<string> = [];
-    locs: Array<string> = [];
-    jts: Array<string> = [];
     position: any;
 
   constructor(private http: Http) { 
-    this.campus = "All Campuses";
-    this.jobType = "All Types";
+    this.campus = "New Orleans, LA";
+    this.jobType = "Staff";
     http.get('./assets/jobs.json').subscribe(values => {
       this.rawData = values.json();
       this.positions = this.rawData;
@@ -36,14 +34,6 @@ export class PositionsListComponent implements OnInit {
           if (this.rawData[this.position].location !== null && this.rawData[this.position].location !== "")
               this.locations.push(this.rawData[this.position].location);
       }
-
-      this.jts = Array.from(new Set(this.jobTypes));
-      this.jobTypes = this.jts.sort();
-      this.jobTypes.unshift('All Types');
-
-      this.locs = Array.from(new Set(this.locations));
-      this.locations = this.locs.sort();
-      this.locations.unshift('All Campuses');
   });
   }
 
